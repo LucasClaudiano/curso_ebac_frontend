99a87fcb2a6952a13b24d18b8a49a95b9c9f46d1
Lucas Matias <85596165+LucasClaudiano@users.noreply.github.com>
2025-05-01 18:46:23 +0200

    adding activity

---
+++ exercicio.js
@@ -0,0 +1,8 @@
+function mutiplicaNumero(n1, n2) {
+    return n1 * n2;
+}
+function saudacao(nome) {
+    return "Boa tarde " + nome;
+}
+console.log(mutiplicaNumero(50, 2));
+console.log(saudacao("Lucas"));
---
+++ exercicio.ts
@@ -0,0 +1,9 @@
+function mutiplicaNumero (n1:number, n2:number){
+    return n1 * n2;
+}
+function saudacao (nome:string){
+    return "Boa tarde "+nome;
+}
+
+console.log(mutiplicaNumero(50,2))
+console.log(saudacao("Lucas"))
